30cf645b25122d3ad7ba07b2a26d4a5c0af34707
Parent Map Deploy <deploy@parentmap.hk>
2026-02-12 08:58:29 +0800

    Add 30 new locations (021-050) - Total 50 locations now

---
+++ data/locations_021_050.ts
@@ -0,0 +1,270 @@
+  {
+    id: "021", name: "慧泊雅 (Whisky)", district: "中環", region: "hk-island",
+    lat: 22.2850, lng: 114.1520, category: "playhouse", indoor: true,
+    ageRange: [2, 3], priceType: "medium", priceDescription: "$100-200",
+    description: "澳洲主題遊樂室，設有動物火車、超市、球池及釣魚遊戲",
+    website: "https://playroo.com.hk/", tips: "適合幼兒",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "北角 King's Road 147-151號 1樓"
+  },
+  {
+    id: "022", name: "多科多科 (DOCODOCO)", district: "將軍澳", region: "nt",
+    lat: 22.3130, lng: 114.2640, category: "playhouse", indoor: true,
+    ageRange: [0, 12], priceType: "high", priceDescription: "$200-400",
+    description: "VR 遊樂場，設有感官沙灘、瀑布滑梯及互動荷花池",
+    website: "https://www.namco.com.hk/docodoco", tips: "科技感強",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "將軍澳 The LOHAS 4樓"
+  },
+  {
+    id: "023", name: "蚊叮叮 (Moskiddo)", district: "黃竹坑", region: "hk-island",
+    lat: 22.2485, lng: 114.1555, category: "playhouse", indoor: true,
+    ageRange: [2, 12], priceType: "high", priceDescription: "$200-400",
+    description: "大型遊樂場，設有無限遊樂區、監督及小食角",
+    website: "https://www.moskiddo.com/", tips: "場地大",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: true, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "黃竹坑 One Island South 13樓"
+  },
+  {
+    id: "024", name: "奇想 (WHIMSY)", district: "灣仔", region: "hk-island",
+    lat: 22.2770, lng: 114.1730, category: "playhouse", indoor: true,
+    ageRange: [0, 12], priceType: "high", priceDescription: "$200-400",
+    description: "遊樂空間，設有 VR 乘騎、碰碰車、雪地仙境及街機",
+    website: "https://www.ysmihw.com.hk/", tips: "適合大童",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "灣仔 Hopewell Centre 1403-1405室"
+  },
+  {
+    id: "025", name: "奇奇遊樂場 (Kids Kids)", district: "九龍灣", region: "kowloon",
+    lat: 22.3230, lng: 114.2080, category: "playhouse", indoor: true,
+    ageRange: [1, 12], priceType: "medium", priceDescription: "$100-200",
+    description: "沉浸式遊樂場，設有 20+ 區如 AR 投影、修車及電動乘騎",
+    website: "https://www.megabox.com.hk/", tips: "MegaBox內",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "九龍灣 MegaBox"
+  },
+  {
+    id: "026", name: "蟲城 (Verm City)", district: "鰂魚涌", region: "hk-island",
+    lat: 22.2876, lng: 114.2120, category: "playhouse", indoor: true,
+    ageRange: [3, 12], priceType: "medium", priceDescription: "$100-200",
+    description: "室內攀岩健身房，設有抱石牆及 Clip-n-Climb",
+    website: "https://www.vermcity.com/", tips: "運動量大",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "鰂魚涌 Kodak House 1, 4樓"
+  },
+  {
+    id: "027", name: "遊樂屋 (The Playhouse)", district: "數碼港", region: "hk-island",
+    lat: 22.2510, lng: 114.1480, category: "playhouse", indoor: true,
+    ageRange: [1, 12], priceType: "medium", priceDescription: "$100-200",
+    description: "基於遊樂的學習空間，設有藝術工作坊及實踐活動",
+    website: "https://www.pplayhouse.com/", tips: "教育導向",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "數碼港 The Arcade 301A"
+  },
+  {
+    id: "028", name: "派對貓遊樂屋 (Party Cat)", district: "堅尼地城", region: "hk-island",
+    lat: 22.2810, lng: 114.1270, category: "playhouse", indoor: true,
+    ageRange: [1, 6], priceType: "medium", priceDescription: "$100-200",
+    description: "恐龍主題遊樂場，設有挖掘機、球池、沙坑及街機",
+    website: "https://partycat.fun/", tips: "恐龍迷必去",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "堅尼地城 Cheung Fat Industrial Building"
+  },
+  {
+    id: "029", name: "沙盒 VR (Sandbox VR)", district: "銅鑼灣/尖沙咀", region: "hk-island",
+    lat: 22.2810, lng: 114.1830, category: "playhouse", indoor: true,
+    ageRange: [6, 12], priceType: "high", priceDescription: "$200-400",
+    description: "沉浸式 VR 世界，適合團體冒險如殭屍戰或海盜任務",
+    website: "https://sandboxvr.com/", tips: "適合大童及青少年",
+    hasBabyRoom: false, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "銅鑼灣 Soundwill Plaza II 23樓"
+  },
+  {
+    id: "030", name: "樹先生 (Mr Tree)", district: "荔枝角/銅鑼灣", region: "kowloon",
+    lat: 22.3370, lng: 114.1480, category: "playhouse", indoor: true,
+    ageRange: [0, 6], priceType: "medium", priceDescription: "$100-200",
+    description: "遊樂室配自助餐廳，設有滑梯、球池及繩橋",
+    website: "https://www.facebook.com/Mr.Tree38HK/", tips: "有自助餐",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: true, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "荔枝角 D2 Place Two 1-2樓"
+  },
+  {
+    id: "031", name: "城市畫廊 (City Gallery)", district: "中環", region: "hk-island",
+    lat: 22.2830, lng: 114.1610, category: "museum", indoor: true,
+    ageRange: [2, 6], priceType: "free", priceDescription: "免費",
+    description: "免費教育遊樂室，設有互動城市設計展覽及 SimCity 遊戲",
+    website: "https://www.citygallery.gov.hk/", tips: "免費入場",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "中環 Edinburgh Place 3號 1樓"
+  },
+  {
+    id: "032", name: "小怪物 (Little Monster)", district: "柴灣", region: "hk-island",
+    lat: 22.2640, lng: 114.2360, category: "playhouse", indoor: true,
+    ageRange: [2, 6], priceType: "medium", priceDescription: "$100-200",
+    description: "遊樂公園，設有波浪滑梯、攀爬、超市角色扮演及火車",
+    website: "https://www.facebook.com/profile.php?id=100090297034671", tips: "角色扮演",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "柴灣 Eltee Building, Ning Foo St 3號"
+  },
+  {
+    id: "033", name: "鯊魚及海盜冒險區", district: "屯門", region: "nt",
+    lat: 22.3860, lng: 113.9810, category: "playhouse", indoor: true,
+    ageRange: [1, 6], priceType: "medium", priceDescription: "$100-200",
+    description: "軟墊遊樂室，設有海盜主題，適合酒店客人及訪客",
+    website: "https://www.goldcoasthotel.com.hk/", tips: "黃金海岸酒店內",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: true, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "屯門黃金海岸 1 Castle Peak Road"
+  },
+  {
+    id: "034", name: "叢林兒童冒險 (Jungle Kidventure)", district: "九龍灣", region: "kowloon",
+    lat: 22.3230, lng: 114.2080, category: "playhouse", indoor: true,
+    ageRange: [2, 6], priceType: "medium", priceDescription: "$100-200",
+    description: "叢林主題遊樂屋，設有彈跳城堡、沙坑及電動車",
+    website: "https://www.facebook.com/jungle-kidventure", tips: "叢林主題",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "九龍灣 Megabox 9樓"
+  },
+  {
+    id: "035", name: "奇妙谷 (Wonder Valley)", district: "深水埗", region: "kowloon",
+    lat: 22.3370, lng: 114.1480, category: "playhouse", indoor: true,
+    ageRange: [2, 6], priceType: "medium", priceDescription: "$100-200",
+    description: "奇幻遊樂場，設有童話區、海盜船、軟墊遊樂及球池",
+    website: "https://www.facebook.com/wondervalley.hk", tips: "童話主題",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "深水埗 Cheung Sha Wan Plaza 3樓 312-317"
+  },
+  {
+    id: "036", name: "E3 俱樂部 (E3 Club)", district: "九龍灣", region: "kowloon",
+    lat: 22.3230, lng: 114.2080, category: "playhouse", indoor: true,
+    ageRange: [0, 6], priceType: "medium", priceDescription: "$100-200",
+    description: "娛樂焦點遊樂場，設有蹦床、球池及巨型滑梯",
+    website: "https://www.e3club.com.hk/", tips: "MegaBox內",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "九龍灣 MegaBox 12樓"
+  },
+  {
+    id: "037", name: "摺紙咖啡廳 (Origami Cafe)", district: "九龍灣", region: "kowloon",
+    lat: 22.3070, lng: 114.2130, category: "restaurant", indoor: true,
+    ageRange: [0, 6], priceType: "medium", priceDescription: "$100-200",
+    description: "咖啡廳配教育遊樂區、玩具及工作坊，提供融合菜餚",
+    website: "https://www.facebook.com/origamikidscafe", tips: "親子餐廳",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: true, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "啟德郵輪碼頭頂層"
+  },
+  {
+    id: "038", name: "兒童樂園遊樂公園 (Kiddoland)", district: "紅磡", region: "kowloon",
+    lat: 22.3050, lng: 114.1880, category: "playhouse", indoor: true,
+    ageRange: [2, 6], priceType: "medium", priceDescription: "$100-200",
+    description: "緊湊遊樂場，設有蹦床、球池、滑梯及軟玩具",
+    website: "https://www.facebook.com/kiddolandplaypark", tips: "紅磡工廈區",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "紅磡 Hang Fung Industrial Building"
+  },
+  {
+    id: "039", name: "奔跑兒童遊樂屋 (Runningkids)", district: "紅磡", region: "kowloon",
+    lat: 22.3050, lng: 114.1880, category: "playhouse", indoor: true,
+    ageRange: [1, 6], priceType: "medium", priceDescription: "$100-200",
+    description: "中心設有球池、滑梯、蹦床、沙區及賽道",
+    website: "https://www.facebook.com/Runningkidsplayhouse", tips: "紅磡區",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "紅磡 Fortune Metropolis"
+  },
+  {
+    id: "040", name: "兒童樂園 (KidZania)", district: "大嶼山", region: "nt",
+    lat: 22.3160, lng: 113.9360, category: "playhouse", indoor: true,
+    ageRange: [3, 12], priceType: "high", priceDescription: "$200-400",
+    description: "迷你城市，適合角色扮演職業如科技、零售等",
+    website: "https://www.kidzania.com.hk/", tips: "11 SKIES內",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: true, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "大嶼山 11 SKIES, 8 Sky City Road East"
+  },
+  {
+    id: "041", name: "三角加 (Triangle Plus)", district: "荃灣", region: "nt",
+    lat: 22.3730, lng: 114.1150, category: "playhouse", indoor: true,
+    ageRange: [3, 12], priceType: "high", priceDescription: "$200-400",
+    description: "冒險攀爬公園，設有牆壁、障礙、球池及洞穴",
+    website: "https://www.triangleplushk.com", tips: "Plaza 88內",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "荃灣 Plaza 88"
+  },
+  {
+    id: "042", name: "兒童科技 (Kidztech)", district: "紅磡", region: "kowloon",
+    lat: 22.3050, lng: 114.1880, category: "playhouse", indoor: true,
+    ageRange: [2, 12], priceType: "medium", priceDescription: "$100-200",
+    description: "寬敞遊樂室，設有感官遊樂、沙坑、真空球池及木玩具",
+    website: "https://www.facebook.com/kidztechhk", tips: "紅磡區",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "紅磡 Harbourfront Landmark Office Level 6樓"
+  },
+  {
+    id: "043", name: "探索者 (The Explorers)", district: "鴨脷洲", region: "hk-island",
+    lat: 22.2400, lng: 114.1550, category: "playhouse", indoor: true,
+    ageRange: [0, 6], priceType: "high", priceDescription: "$200-400",
+    description: "軟墊遊樂區，設有日票及泳池通行",
+    website: "https://www.fullertonhotels.com", tips: "Fullerton Ocean Park酒店內",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: true, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "鴨脷洲 Ocean Drive 3號"
+  },
+  {
+    id: "044", name: "真專家遊樂趣區 (Real Expert)", district: "上環", region: "hk-island",
+    lat: 22.2870, lng: 114.1500, category: "playhouse", indoor: true,
+    ageRange: [2, 12], priceType: "medium", priceDescription: "$100-200",
+    description: "遊樂場，設有滑梯、隧道、街機、沙坑及迷你廚房",
+    website: "https://www.facebook.com/RealExpertPlayFunZone", tips: "上環區",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "上環 Infinitus Plaza 2樓"
+  },
+  {
+    id: "045", name: "AME 體育館 (AME Stadium)", district: "尖沙咀", region: "kowloon",
+    lat: 22.2980, lng: 114.1720, category: "playhouse", indoor: true,
+    ageRange: [12], priceType: "high", priceDescription: "$200-400",
+    description: "電子競技及遊戲場地，設有互動裝置",
+    website: "https://www.ame.gg", tips: "適合青少年",
+    hasBabyRoom: false, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "尖沙咀"
+  },
+  {
+    id: "046", name: "100Fun 樂富", district: "黃大仙", region: "kowloon",
+    lat: 22.3400, lng: 114.1870, category: "playhouse", indoor: true,
+    ageRange: [2, 6], priceType: "medium", priceDescription: "$100-200",
+    description: "一般室內遊樂室，適合休閒活動",
+    website: "https://www.100fun.com.hk/", tips: "樂富區",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "黃大仙樂富"
+  },
+  {
+    id: "047", name: "兒童 Fi 派對室 (Kids Fi)", district: "葵涌", region: "nt",
+    lat: 22.3630, lng: 114.1310, category: "playhouse", indoor: true,
+    ageRange: [2, 12], priceType: "medium", priceDescription: "$100-200",
+    description: "派對導向遊樂場",
+    website: "https://www.kidsfi.com.hk/", tips: "葵涌區",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "葵涌"
+  },
+  {
+    id: "048", name: "快樂時光遊樂屋 (Happy Hours)", district: "深水埗", region: "kowloon",
+    lat: 22.3370, lng: 114.1480, category: "playhouse", indoor: true,
+    ageRange: [2, 6], priceType: "medium", priceDescription: "$100-200",
+    description: "軟墊遊樂，設有迷你車及墊區",
+    website: "https://www.happyhoursplayhouse.com/", tips: "荔枝角區",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "荔枝角"
+  },
+  {
+    id: "049", name: "甜圈遊樂屋 (Donut Playhouse)", district: "尖沙咀", region: "kowloon",
+    lat: 22.2940, lng: 114.1750, category: "playhouse", indoor: true,
+    ageRange: [2, 6], priceType: "medium", priceDescription: "$100-200",
+    description: "遊樂區，設有巨型滑梯及軟區",
+    website: "https://www.k11.com/musea/", tips: "K11 MUSEA內",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "尖沙咀 K11 MUSEA"
+  },
+  {
+    id: "050", name: "石塘咀體育中心", district: "中西區", region: "hk-island",
+    lat: 22.2860, lng: 114.1360, category: "playhouse", indoor: true,
+    ageRange: [3, 12], priceType: "free", priceDescription: "免費",
+    description: "免費政府遊樂場，設有天空主題攀爬及滑梯",
+    website: "https://www.lcsd.gov.hk/", tips: "康文署設施，免費",
+    hasBabyRoom: true, hasStrollerAccess: true, hasRestaurant: false, rainyDaySuitable: true,
+    openingHours: "請查詢官網", address: "石塘咀 Municipal Services Building"
+  }
